11cb2ef70025b82f58d8ff2c5819e573e0269581
finka759 <finka759@gmail.com>
2025-03-19 12:15:48 +0300

    Задача №1. Денежные переводы

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("First App!")
+
+    val tax: Float = 0.0075F
+    val minTax: Int = 35
+    val amount: Int = 10000
+
+    val realTax = if ((amount * tax) > 35) {
+        amount * tax
+    } else {
+        minTax
+    }.toInt()
+
+    print(realTax)
+
 }
